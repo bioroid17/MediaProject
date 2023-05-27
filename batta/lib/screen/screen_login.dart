ee497c1016848b52c1ba6ca11026f71bf36bb6a9
JeongHoon Lee <trace1997@ajou.ac.kr>
2023-05-27 21:54:57 +0900

    minor UI changes

--- batta/lib/screen/screen_login.dart
+++ batta/lib/screen/screen_login.dart
@@ -24,9 +24,13 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
         backgroundColor: const Color(0xFFEFECE5),
         body: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(vertical: height * 0.1),
           child: Center(
             child: Container(
+              margin: EdgeInsets.symmetric(vertical: height * 0.1),
+              padding: EdgeInsets.symmetric(
+                vertical: height * 0.03,
+                horizontal: width * 0.1,
+              ),
               decoration: BoxDecoration(
                 border: Border.all(
                   color: const Color(0xFF0D2065),
@@ -34,7 +38,6 @@ class _LoginScreenState extends State<LoginScreen> {
                 borderRadius: BorderRadius.circular(10),
               ),
               width: width * 0.85,
-              height: height * 0.5,
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -47,7 +50,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                     Container(
                       padding: EdgeInsets.symmetric(
-                        horizontal: width * 0.1,
                         vertical: height * 0.02,
                       ),
                       child: const TextField(
@@ -59,7 +61,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                     Container(
                       padding: EdgeInsets.symmetric(
-                        horizontal: width * 0.1,
                         vertical: height * 0.02,
                       ),
                       child: const TextField(
--- batta/lib/screen/screen_notice.dart
+++ batta/lib/screen/screen_notice.dart
@@ -16,7 +16,7 @@ class _NoticeScreenState extends State<NoticeScreen> {
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(8.0),
-        color: Color.fromARGB(255, 255, 255, 255),
+        color: const Color.fromARGB(255, 255, 255, 255),
         child: ListView.separated(
           scrollDirection: Axis.vertical,
           itemBuilder: (context, index) {
