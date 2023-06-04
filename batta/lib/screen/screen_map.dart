a0ee4a5dabc57bae6512d494d690434d7bbc4536
BambiLee <shlee00218@ajou.ac.kr>
2023-06-04 23:42:32 +0900

    map marker

--- batta/lib/screen/screen_map.dart
+++ batta/lib/screen/screen_map.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({super.key});
+  const MapScreen({Key? key}) : super(key: key);
 
   @override
   State<MapScreen> createState() => _MapScreenState();
@@ -10,10 +10,37 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
+  List<Marker> markers = [];
 
   @override
   void initState() {
     super.initState();
+    addCustomMarkers();
+  }
+
+  void addCustomMarkers() {
+    final List<LatLng> locations = [
+      const LatLng(37.28020628270281, 127.04442741544098), // 아주대 야외 체육관
+      const LatLng(37.28332967800612, 127.04083488004338), // 유신고 야구장
+      const LatLng(37.26174271998503, 127.0257931750296), // 야구연습장
+      const LatLng(37.30022854170339, 127.00958713508943), // 수원KT위즈파크
+    ];
+
+    for (int i = 0; i < locations.length; i++) {
+      final marker = Marker(
+        markerId: MarkerId('marker$i'),
+        position: locations[i],
+        // 추가적인 옵션들 설정 가능
+        // icon: BitmapDescriptor.defaultMarker,
+        // infoWindow: InfoWindow(title: 'Custom Marker $i'),
+      );
+
+      markers.add(marker);
+    }
+
+    setState(() {
+      markers = markers;
+    });
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -32,11 +59,18 @@ class _MapScreenState extends State<MapScreen> {
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: const CameraPosition(
-            target: LatLng(37.5665, 126.9780), // 서울의 위도, 경도
-            zoom: 10.0,
+            target: LatLng(37.28296604008742, 127.0435551162717),
+            zoom: 15.0,
           ),
+          markers: markers.toSet(),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: MapScreen(),
+  ));
 }
